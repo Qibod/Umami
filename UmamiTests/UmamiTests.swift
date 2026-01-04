245bfc718ce1b7b96c996082e577013d0230c2c8
sylvaanas <vijay0510@gmail.com>
2026-01-04 23:58:20 +0900

    Initial Commit

---
+++ UmamiTests/UmamiTests.swift
@@ -0,0 +1,17 @@
+//
+//  UmamiTests.swift
+//  UmamiTests
+//
+//  Created by Vijay R on 1/4/26.
+//
+
+import Testing
+@testable import Umami
+
+struct UmamiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
